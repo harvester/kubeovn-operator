998310e3ae9f1277c50b47d77f39bc2e2237ce43
Gaurav Mehta <gaurav.mehta@suse.com>
2026-02-02 16:02:06 +1100

    minor change to run a separate manager instance for webhooks which does not run leader election. This ensures that webhook is served by all pods and service stays available during upgrades
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    (cherry picked from commit 096c6d2971c1f1328793f895501adbc9f48f0a95)

--- cmd/main.go
+++ cmd/main.go
@@ -208,7 +208,6 @@ func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
-		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "1d231218.my.domain",
@@ -273,12 +272,24 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Node")
 	}
 
+	webhookMgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: "",
+		},
+		HealthProbeBindAddress: "",
+		LeaderElection:         false,
+		WebhookServer:          webhookServer,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
 	// nolint:goconst
-	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err = webhookkubeovnv1.SetupConfigurationWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "Configuration")
-			os.Exit(1)
-		}
+	if err = webhookkubeovnv1.SetupConfigurationWebhookWithManager(webhookMgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Configuration")
+		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
 
@@ -292,7 +303,7 @@ func main() {
 
 	if webhookCertWatcher != nil {
 		setupLog.Info("Adding webhook certificate watcher to manager")
-		if err := mgr.Add(webhookCertWatcher); err != nil {
+		if err := webhookMgr.Add(webhookCertWatcher); err != nil {
 			setupLog.Error(err, "unable to add webhook certificate watcher to manager")
 			os.Exit(1)
 		}
@@ -307,9 +318,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	go func() {
+		setupLog.Info("starting manager")
+		if err := webhookMgr.Start(ctx); err != nil {
+			setupLog.Error(err, "problem running webhook manager instance")
+			os.Exit(1)
+		}
+	}()
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctx); err != nil {
-		setupLog.Error(err, "problem running manager")
+		setupLog.Error(err, "problem running controller manager instance")
 		os.Exit(1)
 	}
+
 }
